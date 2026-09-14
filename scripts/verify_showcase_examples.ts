81ae12273f6f27c4c945f27f666f7a015148c3ed
Jacob Torrång <kopatheonlyone@hotmail.com>
2026-09-14 11:06:03 +0200

    Add event-sourced aggregate to showcase verification

--- scripts/verify_showcase_examples.ts
+++ scripts/verify_showcase_examples.ts
@@ -51,6 +51,7 @@ const abstractionExamples = [
   "derived_structural_diff.blot",
   "staged_request_builder.blot",
   "typed_codec.blot",
+  "event_sourced_aggregate.blot",
 ] as const;
 
 let selectedExamples: readonly string[] = [
