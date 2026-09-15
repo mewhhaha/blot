8a3a936727e54ebf1a1b233a1ea76c094f34c79c
Jacob Torrång <kopatheonlyone@hotmail.com>
2026-09-15 13:00:44 +0200

    Register bidirectional compatibility showcase

--- scripts/verify_showcase_examples.ts
+++ scripts/verify_showcase_examples.ts
@@ -58,6 +58,7 @@ const abstractionExamples = [
   "reversible_updates.blot",
   "typed_traversals.blot",
   "typed_record_projections.blot",
+  "bidirectional_compatibility.blot",
 ] as const;
 
 let selectedExamples: readonly string[] = [
